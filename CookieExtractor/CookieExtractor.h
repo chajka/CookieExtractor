b34ad4c61d8e59c30cf4eb7634d90e1e85abda3f
Чайка <mellanie335@gmail.com>
2017-04-18 21:58:36 +0900

    Initial Commit

---
+++ CookieExtractor/CookieExtractor.h
@@ -0,0 +1,19 @@
+//
+//  CookieExtractor.h
+//  CookieExtractor
+//
+//  Created by Чайка on 4/18/17.
+//  Copyright © 2017 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CookieExtractor.
+FOUNDATION_EXPORT double CookieExtractorVersionNumber;
+
+//! Project version string for CookieExtractor.
+FOUNDATION_EXPORT const unsigned char CookieExtractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CookieExtractor/PublicHeader.h>
+
+

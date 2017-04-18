3cb4af16ba6cb6d604a73b2fc0224018ab0d6f10
Чайка <mellanie335@gmail.com>
2017-04-19 00:39:36 +0900

    Add SafariCookieParser Class into framework.

---
+++ CookieExtractor/BinaryCookieRader/CookieParser.h
@@ -0,0 +1,20 @@
+//
+//  CookieParser.h
+//  binarycookies
+//
+//  Created by Чайка on 3/23/16.
+//  Copyright © 2016 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "binaryreader.h"
+
+@interface CookieParser : NSObject {
+	NSData					*cookieData;
+}
+
+#pragma mark - messages
+- (nonnull id) initWithData:(nonnull NSData *)data;
+
+- (nonnull NSArray<NSHTTPCookie *> *)parseCookies;
+@end
